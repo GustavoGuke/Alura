ee4da618b760ad6a31b4971fa2431304d0b74f9f
Gustavo Silva <gustavo.athenas13@gmail.com>
2023-08-01 21:36:59 -0300

    metodos read e create

--- formacoes/node-api/c1-orm-sequelize/api/controllers/PessoaControllers.js
+++ formacoes/node-api/c1-orm-sequelize/api/controllers/PessoaControllers.js
@@ -11,6 +11,26 @@ class PessoaController {
             return res.json(error.message)
         }
     }
+
+    static async pegaUmaPessoa(req, res){
+        const {id} = req.params
+        try {
+            const umaPessoa = await database.Pessoas.findOne({where: { id : Number(id) }})
+            return res.json(umaPessoa)
+        } catch (error) {
+            return res.json(error.message)
+        }
+    }
+
+    static async criaNovaPessoa(req, res){
+        const novaPessoa = req.body
+        try {
+            const pessoaCriada = await database.Pessoas.create(novaPessoa)
+            return res.json(pessoaCriada)
+        } catch (error) {
+            return res.json(error.message)
+        }
+    }
 }
 
 module.exports = PessoaController
--- formacoes/node-api/c1-orm-sequelize/api/routes/pessoasRoutes.js
+++ formacoes/node-api/c1-orm-sequelize/api/routes/pessoasRoutes.js
@@ -5,5 +5,7 @@ const PessoaController = require('../controllers/PessoaControllers')
 const router = Router()
 
 router.get('/pessoas', PessoaController.pegaTodasAsPessoas)
+router.get('/pessoas/:id', PessoaController.pegaUmaPessoa)
+router.post('/pessoas', PessoaController.criaNovaPessoa)
 
 module.exports = router
