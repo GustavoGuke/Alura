4b02dcc5d7fc979d248dfb03cdaf27bc1992a8de
Gustavo da Silva de Lima <gustavo.lima@poleoduto.msft>
2023-08-09 17:19:44 -0300

    organizando código

--- formacoes/react-js/c1-organo/organo/src/App.js
+++ formacoes/react-js/c1-organo/organo/src/App.js
@@ -6,23 +6,6 @@ function App() {
   return (
     <div className="App">
       <Banner />
-<<<<<<< HEAD
-=======
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
->>>>>>> 01d89dc677be34742e6cbb0edcff6d527cbb60b1
     </div>
   );
 }
--- formacoes/react-js/c1-organo/organo/src/components/Banner/Banner.js
+++ formacoes/react-js/c1-organo/organo/src/components/Banner/Banner.js
@@ -2,13 +2,8 @@ import './Banner.css'
 
 function Banner(){
     return (
-<<<<<<< HEAD
         <header className="banner">
             <img  src="./images/banner.png"/>
-=======
-        <header className='banner'>
-            <img src="./images/banner.png" alt="banner app"/>
->>>>>>> 01d89dc677be34742e6cbb0edcff6d527cbb60b1
         </header>
     )
 }
