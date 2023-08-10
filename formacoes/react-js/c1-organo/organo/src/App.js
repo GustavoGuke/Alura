ccbd7cd19f9abad48d5c8b79293d028876f6b264
Gustavo Silva <gustavo.athenas13@gmail.com>
2023-08-09 23:47:20 -0300

    componente campoTexto

--- formacoes/react-js/c1-organo/organo/src/App.js
+++ formacoes/react-js/c1-organo/organo/src/App.js
@@ -1,11 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 import Banner from './components/Banner/Banner';
+import CampoTexto from './components/CampoTexto/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
+      <CampoTexto 
+      label="NOME"
+      placeholder="Digite seu Nome"
+      />
+      <CampoTexto
+        label="CARGO"
+        placeholder="Digite seu Cargo"
+      />
+      <CampoTexto
+        label="IMAGEM"
+        placeholder="coloque sua imagem"
+      />
     </div>
   );
 }
