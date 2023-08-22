f895c7707360350babe52abec8d4c9eafc15b145
Gustavo Silva <gustavo.athenas13@gmail.com>
2023-08-22 00:14:14 -0300

    c2-organo - novas funcionalidades - botão excluir

---
+++ formacoes/react-js/c2-organo/src/componentes/Time/index.js
@@ -0,0 +1,26 @@
+import Colaborador from '../Colaborador'
+import './Time.css'
+
+const Time = (props) => {
+    const css = { backgroundColor: props.corSecundaria }
+
+    return (
+        (props.colaboradores.length > 0) ? <section className='time' style={css}>
+            <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map( (colaborador,indice )=> {
+                    return <Colaborador 
+                    corDeFundo={props.corPrimaria} 
+                    key={indice} 
+                    nome={colaborador.nome} 
+                    cargo={colaborador.cargo} 
+                    imagem={colaborador.imagem}
+                    aoDeletar={props.aoDeletar} /> 
+                })}
+            </div>
+        </section> 
+        : ''
+    )
+}
+
+export default Time
