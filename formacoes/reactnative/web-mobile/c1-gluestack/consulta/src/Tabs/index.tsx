af279a6697550e945a9cba2a63e56080b28a2bdc
Gustavo Silva <gustavo.athenas13@gmail.com>
2024-02-14 19:53:50 -0300

    navegação Tabs - icones

--- formacoes/reactnative/web-mobile/c1-gluestack/consulta/src/Tabs/index.tsx
+++ formacoes/reactnative/web-mobile/c1-gluestack/consulta/src/Tabs/index.tsx
@@ -1,4 +1,6 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Home, Calendar } from 'lucide-react-native'
+import { Icon} from "@gluestack-ui/themed";
 
 import TelaPrincipal from "./TelaPrincipal";
 import Consultas from "./Consultas";
@@ -7,14 +9,34 @@ const Tab = createBottomTabNavigator()
 
 export default function Tabs() {
     return (
-       <Tab.Navigator>
+       <Tab.Navigator
+        screenOptions={{
+          tabBarStyle: {
+            backgroundColor: "#002851"
+          },
+          tabBarActiveTintColor: "#339cff",
+          tabBarInactiveTintColor: "#fff"
+        }}
+       >
          <Tab.Screen
            name="Principal"
            component={TelaPrincipal}
+           options={{
+            headerShown: false,
+            tabBarIcon: ({color}) => (
+              <Icon as={Home} size="lg"  color={color}/>
+            )
+           }}
            />
            <Tab.Screen
            name="Consultas"
            component={Consultas}
+          options={{
+            headerShown: false,
+            tabBarIcon: ({color}) => (
+              <Icon as={Calendar} size="lg" color={color} />
+            )
+          }}
            />
            </Tab.Navigator>
     )
