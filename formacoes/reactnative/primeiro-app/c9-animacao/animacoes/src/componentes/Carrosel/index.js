aae18329baf8b028c7b49c1a7e79f904df16a334
Gustavo da Silva de Lima <gustavo.lima@poleoduto.msft>
2024-02-20 14:54:20 -0300

    Animação tela de login do app aluramed

--- formacoes/reactnative/primeiro-app/c9-animacao/animacoes/src/componentes/Carrosel/index.js
+++ formacoes/reactnative/primeiro-app/c9-animacao/animacoes/src/componentes/Carrosel/index.js
@@ -1,11 +1,30 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { View, FlatList, Image } from "react-native";
 import styles from "./styles";
 
 export function Carrosel({ itens }) {
     const carroselRef = useRef()
+    const [indice, setIndice] = useState(0)
 
+    function alteraPosicaoCarrosel(){
+        if(indice < itens.length -1){
+            setIndice(indice + 1)
+        }
+        else {
+            setIndice(0)
+        }
+    }
     
+    useEffect(() => {
+        carroselRef.current.scrollToIndex({index: indice})
+
+        const intervalo = setInterval(() => {
+            alteraPosicaoCarrosel()
+        }, 2000)
+
+        return () => clearInterval(intervalo)
+
+    }, [indice])
     return (
         <View style={styles.container}>
             <FlatList
@@ -13,13 +32,14 @@ export function Carrosel({ itens }) {
                 horizontal
                 showsHorizontalScrollIndicator={false}
                 keyExtractor={item => item.id}
-                renderItem={({ item }) => (
+                renderItem={({ item, index }) => (
                     <Image
                         source={item.imagem}
-                        style={styles.imagem}
+                        style={[styles.imagem, index == itens.length -1 ? {marginRight: 200}: null]}
                         resizeMode="contain"
                     />
                 )}
+                ref={carroselRef}
             />
         </View>
     )
