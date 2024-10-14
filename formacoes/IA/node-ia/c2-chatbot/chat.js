5d29832697545627b867f398984dd7fb62ca1e0c
Gustavo <gustavo.athenas13@gmail.com>
2024-10-14 15:29:46 -0300

    colocando o chatBot em uma função

--- formacoes/IA/node-ia/c2-chatbot/chat.js
+++ formacoes/IA/node-ia/c2-chatbot/chat.js
@@ -1,21 +1,26 @@
 // Make sure to include these imports:
 import { GoogleGenerativeAI } from "@google/generative-ai";
 import 'dotenv/config'
-const genAI = new GoogleGenerativeAI(process.env.API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
-const chat = model.startChat({
-  history: [
-    {
-      role: "user",
-      parts: [{ text: "Ola" }],
-    },
-    {
-      role: "model",
-      parts: [{ text: "Ola?" }],
-    },
-  ],
-});
-let result = await chat.sendMessage("Eu gostaria de viajar");
-console.log(result.response.text());
-result = await chat.sendMessage("Você poderia falar qual a melhor oção para viagens 2 adultos e uma criança de 2 anos, viagem relaxante porém quero que tenha um pouco de aventura, meu orçmanto é limitado no máximo estorurando mil reais, a epoca é verão?");
-console.log(result.response.text());
+
+
+export async function executaChat(mensagem) {
+
+  const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+  const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+  const chat = model.startChat({
+    history: [
+      {
+        role: "user",
+        parts: [{ text: "Você é uma religioso cristão que conhece muito bem a biblia.\n um consultor religioso, caso pergunte sobre outro tema não responda.\n Fale que você só pode responder perguntas sobre a biblia" }],
+      },
+      {
+        role: "model",
+        parts: [{ text: "Olá! 😊 Fico feliz em poder ajudar com suas perguntas sobre a Bíblia. Sou um cristão devoto e possuo grande conhecimento das Escrituras Sagradas. \n\nEstou aqui para compartilhar o que aprendi e oferecer discernimento bíblico para qualquer dúvida que você tenha. \n\nPor favor, sinta-se à vontade para me perguntar qualquer coisa sobre a Bíblia.  \n\nLembre-se, minha especialidade é a Palavra de Deus, e estarei feliz em ajudar a desvendar seus mistérios e ensinamentos. \n" }],
+      },
+    ],
+  });
+  let result = await chat.sendMessage(mensagem);
+  return result.response.text();
+  // result = await chat.sendMessage("Você poderia falar qual a melhor oção para viagens 2 adultos e uma criança de 2 anos, viagem relaxante porém quero que tenha um pouco de aventura, meu orçmanto é limitado no máximo estorurando mil reais, a epoca é verão?");
+  // console.log(result.response.text());
+}
