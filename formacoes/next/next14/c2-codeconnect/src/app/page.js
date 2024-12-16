1217b6ddff300d59e48cc9262d460842a15a5a82
gusta <gustavo.athenas13@gmail.com>
2024-12-16 07:56:35 -0300

    prisma client - page de error

--- formacoes/next/next14/c2-codeconnect/src/app/page.js
+++ formacoes/next/next14/c2-codeconnect/src/app/page.js
@@ -3,15 +3,20 @@ import logger from "@/logger"
 
 import styles from './page.module.css'
 import Link from "next/link"
+import { prisma } from "../../prisma/db"
 
 async function getAllPosts (page) {
-  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
-  if (!response.ok) {
-    logger.error('Ops, alguma coisa correu mal')
-    return []
+  try {
+    const posts = await prisma.post.findMany({
+      include: {
+        author: true
+      }
+    })
+    return { data: posts, prev: null, next: null }
+  } catch (error) {
+    logger.error('Falaha ao obeter posts', { error })
+    return { data: [], prev: null, next: null }
   }
-  logger.info('Posts obtidos com sucesso')
-  return response.json()
 }
 
 export default async function Home({ searchParams }) {
