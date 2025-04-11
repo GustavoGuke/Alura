65f30cd8c16eafed25679076857fdcc57c6b0f20
Gustavo da Silva de Lima <gustavo.lima@poleoduto.msft>
2025-04-11 17:14:45 -0300

    mostrando comentário de comentário

--- formacoes/next/next14/c3-code-connect/src/components/Replies/index.jsx
+++ formacoes/next/next14/c3-code-connect/src/components/Replies/index.jsx
@@ -2,9 +2,11 @@
  
  import { useState } from 'react'
  import styles from './replies.module.css'
+import { Comment } from '../Comment'
+import { ReplyModal } from '../ModalReply'
  
- export const Replies = () => {
- 
+ export const Replies = ({comment}) => {
+    console.log(comment.children)
      const [showReplies, setShowReplies] = useState(false)
  
      return (<div className={styles.container}>
@@ -16,6 +18,17 @@
              >
                  {showReplies ? 'Ocultar' : 'Ver'} respostas
              </button>
+             {
+                showReplies && <ul>
+                    {
+                        comment.children.map(reply => {
+                        return <li>
+                            <Comment comment={reply} />
+                            <ReplyModal comment={reply}/>
+                        </li>})
+                    }
+                </ul>
+             }
          </div>
      </div>)
  }
